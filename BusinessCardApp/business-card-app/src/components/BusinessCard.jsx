5932462e5ae4993d6893a1b2821c3b4283403af8
Angelika Ervasti <angelika.ervasti@hotmail.fi>
2025-03-19 10:14:47 +0200

    Add BusinessCard.jsx

---
+++ BusinessCardApp/business-card-app/src/components/BusinessCard.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import "./Card.css";
+import abbLogo from "../assets/ABB.png";
+
+const BusinessCard = ({ name, education, jobTitle, company, email }) => {
+  const [showEmail, setShowEmail] = useState(false);
+
+  const toggleEmailVisibility = () => {
+    setShowEmail(!showEmail);
+  };
+
+  return (
+    <div className="businessCard">
+      <img src={abbLogo} alt="ABB Logo" className="logo" />
+      <h2>{name}</h2>
+      <p>{education}</p>
+      <p>{jobTitle}</p>
+      <p>{company}</p>
+      <button onClick={toggleEmailVisibility}>Show Contact Details</button>
+      {showEmail && <p>{email}</p>}
+    </div>
+  );
+};
+
+export default BusinessCard;
